610b736eb2aa7d764320baa0c0da599cffc1cfa8
cmary2023 <121573716+cmary2023@users.noreply.github.com>
2023-04-05 18:40:49 +0100

    first commit

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React  from "react";   
+
+// component who return a card
+const Card = () => {
+return (
+    <div>
+      <img  alt ='robots' src ='' />
+      <div>
+        <h2>Jane Doe</h2>
+        <p>jane.doe@gmail.com</p>
+      </div>
+      </div>
+)
+
+}
+export { Card };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import ReactDOM from 'react-dom/client';// render to a particular element in the DOM using the React DOM library
+import './index.css';
+import {Card}  from './Card';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+
+const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <Card/>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
